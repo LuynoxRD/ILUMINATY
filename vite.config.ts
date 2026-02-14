8c932fe93e6528f878e1c3d92b0d2614a2aaaac8
LuynoxRD <luiscastillo.rd@gmail.com>
2026-02-14 19:52:59 -0400

    config: agregar base path para despliegue en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  // Esta línea es vital para que GitHub Pages cargue los assets correctamente
+  base: '/ILUMINATY/', 
   plugins: [vue()],
   resolve: {
     alias: {
