e97425e872bc89e5d269c603a5843a5e95f5f5a6
LuynoxRD <luiscastillo.rd@gmail.com>
2026-02-14 21:43:40 -0400

    fix: cambiar router a modo hash para renderizar vistas en github pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+// 1. Cambiamos createWebHistory por createWebHashHistory
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './style.css'
 
@@ -23,7 +24,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  // 2. Activamos el modo Hash
+  history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     return { top: 0, behavior: 'smooth' }
